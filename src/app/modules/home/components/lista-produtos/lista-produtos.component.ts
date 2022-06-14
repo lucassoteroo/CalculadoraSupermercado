732de45ca0aeca71c04caf11a60b93ca1b4a513d
lucassoteroo <lucassoteroo14@gmail.com>
2022-06-13 21:22:18 -0300

    criando component e dando funcionalidade

--- src/app/modules/home/components/lista-produtos/lista-produtos.component.ts
+++ src/app/modules/home/components/lista-produtos/lista-produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaProdutosService } from '../../services/lista-produtos.service';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProdutosComponent implements OnInit {
 
-  constructor() { }
+  public listaProdutos: Array<{nomeProduto: string, valorProduto: number}> = []
+  constructor(private listaProdutosService: ListaProdutosService) { }
 
   ngOnInit(): void {
+    this.renderizarLista()
+    this.listaProdutosService.emitEvent.subscribe(
+      res => alert(`${res} foi adicionado a lista de compras`)
+    )
   }
 
+  public renderizarLista() {
+    if (this.listaProdutos) {
+      this.listaProdutos = this.listaProdutosService.mostrarListaProdutos()
+    }
+  }
+
+  public apagarItemLista(item: any) {
+    this.listaProdutos.splice(item, 1)
+  }
 }
--- src/app/modules/home/services/lista-produtos.service.ts
+++ src/app/modules/home/services/lista-produtos.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaProdutosService {
 
-  private listaProdutos: Array<{nomeProduto: string, valorProduto: number}> = [{nomeProduto: 'Arroz', valorProduto: 3}]
+  public emitEvent = new EventEmitter
+  private listaProdutos: Array<{ nomeProduto: string, valorProduto: number }> = [{ nomeProduto: 'Arroz', valorProduto: 3 }]
+  
   constructor() { }
 
   public mostrarListaProdutos() {
@@ -13,7 +15,12 @@ export class ListaProdutosService {
   }
 
   public addListaProdutos(nome: string, valor: number) {
-    return this.listaProdutos.push({nomeProduto: nome, valorProduto: valor})
+    this.listaAlerta(nome)
+    return this.listaProdutos.push({ nomeProduto: nome, valorProduto: valor })
+  }
+
+  public listaAlerta(value: string) {
+    return this.emitEvent.emit(value)
   }
 
 }
